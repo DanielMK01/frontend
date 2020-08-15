408addc601f448134a1d31b0f3166787c662c852
DanielMK01 <daniel.mj.kim1@gmail.com>
2020-08-15 15:24:07 -0700

    added map jpg

---
+++ frontend/MapScroller.java
@@ -0,0 +1,22 @@
+package frontend;
+
+import javax.swing.*;
+
+public class MapScroller {
+    public static void main(String[] args) {
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new JFrame("Test");
+                frame.setSize(500, 400);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });   
+        
+    }
+
+    public MapScroller() {
+        
+    }
+}
